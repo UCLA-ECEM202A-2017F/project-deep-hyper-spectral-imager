5a4b2c971f2baef72016f7de66f821b104e51078
Shoban <shobannarayan@gmail.com>
2017-12-11 22:03:39 -0800

    New script beamformer

--- Matrix/Scripts/basic_beamformer.py
+++ Matrix/Scripts/basic_beamformer.py
@@ -9,24 +9,29 @@ Loads the three sources test data set, analyzes them and generates a
 map of the three sources.
 
 """
-def format_coord(x, y):
-     z = x+y
-     return 'x=%1.4f, y=%1.4f, z=%1.4f'%(x, y, z)
-
 
 from os import path
 import acoular
 from pylab import figure, plot, axis, imshow, colorbar, show
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import sys
 
+def format_coord(x, y):
+     z = x+y
+     return 'x=%1.4f, y=%1.4f, z=%1.4f'%(x, y, z)
+
+FolderName = sys.argv[1]
+datafile = FolderName+'.h5'
 micgeofile = path.join(path.split(acoular.__file__)[0],'xml','mic_geometry.xml')
-datafile = '-35,75.h5'
+
 
 mg = acoular.MicGeom( from_file=micgeofile )
-ts = acoular.TimeSamples( name='-35,75.h5' )
+ts = acoular.TimeSamples( name=datafile )
 ps = acoular.PowerSpectra( time_data=ts, block_size=128, window='Hanning' )
-#ps = acoular.EigSpectra( time_data=ts, block_size=128, window='Hanning' )
-rg = acoular.RectGrid( x_min=-.715, x_max=.715, y_min=-.44, y_max=.6, z=0.6, \
+
+rg = acoular.RectGrid( x_min=-.715, x_max=.715, y_min=-.44, y_max=.6, z=1, \
 increment=0.001 )
 bb = acoular.BeamformerBase( freq_data=ps, grid=rg, mpos=mg )
 #bb = acoular.BeamformerCapon( freq_data=ps, grid=rg, mpos=mg,c=346.04 )
@@ -35,10 +40,10 @@ bb = acoular.BeamformerBase( freq_data=ps, grid=rg, mpos=mg )
 
 pm = bb.synthetic( 8000, 3 )
 Lm = acoular.L_p( pm )
-#imshow( Lm.T, origin='lower', vmin=Lm.max()-5, extent=rg.extend(), \
-#interpolation='bicubic')
-#colorbar()
-plt.imsave('-35,75', Lm.T, origin='lower',vmin=Lm.max()-3,cmap=plt.cm.get_cmap('Blues', 6))
+
+plt.imsave(FolderName, Lm.T, origin='lower',vmin=Lm.max()-3,cmap=plt.cm.get_cmap('Blues', 6))
+
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.imshow( Lm.T,origin='lower',vmin=Lm.max()-3,extent=rg.extend(),cmap=plt.cm.get_cmap('Blues', 6),interpolation='bicubic')
